9fe044936497093d204ff040ddded95e0235c780
ZingaG <trad_bar@mail.ru>
2023-05-24 20:20:21 +0300

    Task 2 completed

--- main.py
+++ main.py
@@ -3,31 +3,57 @@
 # На столе лежат n монеток. Некоторые из них лежат вверх решкой, а некоторые – гербом. Определите минимальное число монеток, которые нужно перевернуть, 
 # чтобы все монетки были повернуты вверх одной и той же стороной. Выведите минимальное количество монет, которые нужно перевернуть
 
-import random
+# import random
 
-money = []
+# money = []
 
-n = int(input("Enter n = "))
-if n <= 0:
-    print("Incorrect input")
-else:
-    orel = 0
-    reshka = 0
-    spisok = ["orel","reshka"]
+# n = int(input("Enter n = "))
+# if n <= 0:
+#     print("Incorrect input")
+# else:
+#     orel = 0
+#     reshka = 0
+#     spisok = ["orel","reshka"]
 
-    for i in range(n):
-        money.append(random.choice(spisok))
+#     for i in range(n):
+#         money.append(random.choice(spisok))
 
-        if money[i] == "reshka":
-            reshka += 1
-        else:
-            orel += 1
+#         if money[i] == "reshka":
+#             reshka += 1
+#         else:
+#             orel += 1
     
-        print(money[i])
+#         print(money[i])
 
-    print(f"Reshka quantity = {reshka}, Orel quantity = {orel}")
+#     print(f"Reshka quantity = {reshka}, Orel quantity = {orel}")
 
-    if orel >= reshka:
-        print(f"Need to turn over {reshka} coins")
-    else:
-        print(f"Need to turn over {orel} coins")
+#     if orel >= reshka:
+#         print(f"Need to turn over {reshka} coins")
+#     else:
+#         print(f"Need to turn over {orel} coins")
+
+# Task 2
+
+# Петя и Катя – брат и сестра. Петя – студент, а Катя – школьница. Петя помогает Кате по математике. 
+# Он задумывает два натуральных числа X и Y (X,Y≤1000), а Катя должна их отгадать. 
+# Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P. Помогите Кате отгадать задуманные Петей числа.
+
+import math
+
+x = int(input("x = "))
+y = int(input("y = "))
+
+s = x + y
+p = x * y
+
+x = 0 # Обнуляем для честности :D
+y = 0 # Обнуляем для честности :D
+
+if (x > 1000) or (y > 1000):
+    print("Incorrect input!")
+else:
+    d = (s * s) - (4 * p)
+    d = math.sqrt(d)
+    y = (s + d) / 2
+    x = (s - d) / 2
+    print(f"Загаданные числа = {x}, {y}")
